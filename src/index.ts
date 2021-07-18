cf13bbbb5e4b05e6bf9fb01cb1ad96436a58c332
fauzanrozzaqo <rizkianakbar4@gmail.com>
2021-07-19 01:27:54 +0700

    fix error cors

--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,67 @@
-import { verify } from 'jsonwebtoken';
-import 'dotenv/config';
-import 'reflect-metadata';
-import express = require('express');
-import { ApolloServer } from 'apollo-server-express';
-import { createConnection } from 'typeorm';
-import { User } from './entity/User';
-import { buildSchema } from 'type-graphql';
-import { UserResolver } from './resolvers/UserResolver';
-import cookieParser = require('cookie-parser');
-import { createAccessToken, createRefreshToken } from './auth';
-import { sendRefreshToken } from './sendRefreshToken';
+import { verify } from "jsonwebtoken";
+import "dotenv/config";
+import "reflect-metadata";
+import express = require("express");
+import { ApolloServer } from "apollo-server-express";
+import { createConnection } from "typeorm";
+import { User } from "./entity/User";
+import { buildSchema } from "type-graphql";
+import { UserResolver } from "./resolvers/UserResolver";
+import cookieParser = require("cookie-parser");
+import { createAccessToken, createRefreshToken } from "./auth";
+import { sendRefreshToken } from "./sendRefreshToken";
+import cors = require("cors");
 
 (async () => {
-	const app = express();
-	app.use(cookieParser());
+  const app = express();
+  app.use(
+    cors({
+      origin: "http://localhost:3000",
+      credentials: true,
+    })
+  );
+  app.use(cookieParser());
+  app.get("/", (req, res) => res.send("Hello world"));
+  app.post("/refresh_token", async (req, res) => {
+    const token = req.cookies.jid;
+    if (!token) {
+      return res.send({ ok: false, accessToken: "" });
+    }
 
-	app.get('/', (req, res) => res.send('Hello world'));
+    let payload = null;
+    try {
+      payload = verify(token, process.env.REFRESH_TOKEN_SECRET);
+    } catch (err) {
+      console.log(err);
+      return res.send({ ok: false, accessToken: "" });
+    }
 
-	app.post('/refresh_token', async (req, res) => {
-		const token = req.cookies.jid;
-		if (!token) {
-			return res.send({ ok: false, accessToken: '' });
-		}
+    const user = await User.findOne({ id: payload.userId });
+    if (!user) {
+      return res.send({ ok: false, accessToken: "" });
+    }
 
-		let payload = null;
-		try {
-			payload = verify(token, process.env.REFRESH_TOKEN_SECRET);
-		} catch (err) {
-			console.log(err);
-			return res.send({ ok: false, accessToken: '' });
-		}
+    if (user.tokenVersion !== payload.tokenVersion) {
+      return res.send({ ok: false, accessToken: "" });
+    }
 
-		const user = await User.findOne({ id: payload.userId });
-		if (!user) {
-			return res.send({ ok: false, accessToken: '' });
-		}
+    sendRefreshToken(res, createRefreshToken(user));
 
-		if (user.tokenVersion !== payload.tokenVersion) {
-			return res.send({ ok: false, accessToken: '' });
-		}
+    return res.send({ ok: true, accessToken: createAccessToken(user) });
+  });
 
-		sendRefreshToken(res, createRefreshToken(user));
+  await createConnection();
 
-		return res.send({ ok: true, accessToken: createAccessToken(user) });
-	});
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [UserResolver],
+    }),
+    context: ({ req, res }) => ({ req, res }),
+  });
 
-	await createConnection();
+  apolloServer.applyMiddleware({ app, cors: false });
 
-	const apolloServer = new ApolloServer({
-		schema: await buildSchema({
-			resolvers: [UserResolver],
-		}),
-		context: ({ req, res }) => ({ req, res }),
-	});
-
-	apolloServer.applyMiddleware({ app });
-
-	app.listen(4000, () => {
-		console.log('express listening on ' + 4000 + ' port');
-	});
+  app.listen(4000, () => {
+    console.log("express listening on " + 4000 + " port");
+  });
 })();
